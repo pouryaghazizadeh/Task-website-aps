67e0a88064ac46f37bcaa3acdf080dc3010c1d74
mahjan <mahjannazari@gmail.com>
2022-03-17 15:03:08 +0330

    added new code

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Container } from "@mui/material";
 import Navbar from "./components/layout/Navbar";
-import Login from "./components/login/Login";
-import NotFound from "./components/pages/NotFound";
-import AddTodoForm from "./components/TodoList/AddTodoForm";
-import Home from "./components/pages/HomePage/Home";
+import Login from "./pages/login/Login";
+import NotFound from "./pages/NotFound";
+import AddTodoForm from "./pages/TodoList/AddTodoForm";
+import Home from "./pages/HomePage/Home";
 
 function App() {
   return (
--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
 import homeRaducer from "./homeSlice";
-// import userReducer from "./userSlice"
+import userReducer from "./userSlice"
 
 export default configureStore({
   reducer: {
     todos: todoReducer,
     home: homeRaducer,
-    // user: userReducer,
+    user: userReducer,
   },
 });
